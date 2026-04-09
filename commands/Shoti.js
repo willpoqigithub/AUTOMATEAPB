0e913912cdea64589e0ed71b8051ee5aada7b8a5
Willydev <166766848+willpoqigithub@users.noreply.github.com>
2026-04-09 08:19:42 +0800

    Add Shoti.js with video metadata structure

---
+++ commands/Shoti.js
@@ -0,0 +1,15 @@
+{
+  "status": true,
+  "result": {
+    "author": "yazky",
+    "title": "No title",
+    "cover_image": "https://p16-common-sign.tiktokcdn-us.com/tos-alisg-p-0037/3109784b56664eaebf84dc06924c0461_1721139857~tplv-tiktokx-cropcenter-q:300:400:q72.jpeg?dr=8596&refresh_token=8baac7be&x-expires=1775779200&x-signature=uvJtJMZFdGFwCJmFSjyJui4WsFo%3D&t=bacd0480&ps=933b5bde&shp=d05b14bd&shcp=1d1a97fc&idc=useast5&biz_tag=tt_video&s=AWEME_DETAIL&sc=cover",
+    "shotiurl": "https://v16m.tiktokcdn-us.com/0b7eb8184ff68bb125c179950cec459e/69d74468/video/tos/alisg/tos-alisg-pve-0037c001/oMBpWAf3oUQSXEEmilFfBBgW6gXpJ5IvRBDsEk/?a=1233&bti=OUBzOTg7QGo6OjZAL3AjLTAzYCMxNDNg&&bt=899&ft=4bUg3MX_8Zmo015m7x4jVJWtYdWrKsd.&mime_type=video_mp4&rc=NzVmaTRlPDw2NDxnOjs1Z0BpanRpcnA5cnM7dDMzODczNEBjMmIyYTIwXzAxMzFeNDRfYSNwczJfMmQ0MV9gLS1kMTFzcw%3D%3D&vvpl=1&l=2026040900165755E3CF415A4D1C34A3E3&btag=e000f8200",
+    "cover": "https://p16-common-sign.tiktokcdn-us.com/tos-alisg-avt-0068/99147f86e629fbc4685471ab17f139b3~tplv-tiktokx-cropcenter-q:300:300:q70.jpeg?dr=8834&idc=useast5&ps=87d6e48a&refresh_token=84bce5c1&s=AWEME_DETAIL&sc=avatar&shcp=1d1a97fc&shp=d05b14bd&t=223449c4&x-expires=1775779200&x-signature=58%2F%2FJSDKWexKxUd6hIazMYH1%2Ffc%3D",
+    "username": "gwydizon",
+    "nickname": "Gwy",
+    "duration": 15,
+    "region": "PH",
+    "total_vids": 2447
+  }
+}
